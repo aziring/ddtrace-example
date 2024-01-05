85d9dbb0741f7a007286c03e57ee830f2d7aa675
Amy Ziring <amy.ziring@okta.com>
2024-01-05 15:21:20 -0500

    initial release

---
+++ db_connect.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const MongoClient = require('mongodb').MongoClient;
+
+
+const url = 'mongodb://127.0.0.1:27017/';
+const client = new MongoClient(url);
+
+const connectToMongo = async () => {
+    console.log("in connect to moongo");
+	try {
+		// Use connect method to     connect to the Server
+		await client.connect();
+		console.log("Connected successfully to server");
+
+		const db = await client.db('local');
+        const cursor = await db.collection("metadata").find({id: 1}, {title: 1, id: 0, _id: 0});
+
+        await cursor.forEach(console.log);
+
+		// Perform further operations on the database here
+
+	} catch (err) {
+		console.log(err);
+	} finally {
+		await client.close();
+	}
+}
+
+module.exports = {
+    connectToMongo,
+  };
+  
